7d765fe812dd2e7f3470d29c8c25e379bcabbdbb
louisgourinchas <louis.gourinchas@gmail.com>
2021-03-05 16:55:08 +0100

    commit

--- ts/server.ts
+++ ts/server.ts
@@ -3,6 +3,8 @@ import multer from 'multer';
 import commandLineArgs, { OptionDefinition } from 'command-line-args';
 import { check, validationResult, param } from 'express-validator';
 import path from "path";
+import fetch from 'node-fetch'
+
 const upload = multer({ dest: '../uploads/' }); // Pour les données form-data et multipart form data
 
 const app = express();
@@ -26,16 +28,55 @@ app.get('/api/addition', upload.none(), [check('a').isNumeric(), check('b').isNu
     if (errors.isEmpty()) {
         const a = +req.body.a;
         const b = +req.body.b;
-        res.status(200).send( `${a + b}` );
+        res.status(200).send( `${a + b}\n` );
     } else {
         res.status(422).json({ errors: errors.array()});
+    }
+});
+
+app.get('/proxy', [check('url').isString()], async (req, res) => {
+    const errors = validationResult(req);
+    if (errors.isEmpty()){
+        const URL = req.body.url
+        console.log("\non veut " + URL + "\n\n");
+
+        //res.json({code:'#WIP'});
+        try {
+            const R = await fetch(URL);
+            res.status(R.status).send(await R.text() );
+        } catch (err) {
+            res.status(500).json({errors: err});
+        }
+
+    } else {
+        res.status(444).json({ errors: errors.array()});
+    }
+});
+
+app.get('/tmdb/search', [check('query').isString()], async (req, res) => {
+    const errors = validationResult(req);
+    if (errors.isEmpty()){
+
+        const query = req.body.query;
+
+        try {
+            const key = "9e0b7e444f3a564140d56af2346a8815";
+            const adress = `https://api.themoviedb.org/3/search/movie?api_key=${key}&query=${encodeURIComponent(query)}`;
+            const R = await fetch(adress);
+            res.status(R.status).json(await R.json() );
+        } catch (err) {
+            res.status(500).json({errors: err});
+        }
+
+    } else {
+        res.status(444).json({ errors: errors.array()});
     }
 });
 
 app.post('/api/upload', upload.none() );
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Hello World!\n');
 });
 
 
